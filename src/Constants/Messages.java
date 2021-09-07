dc497a60d2370c5cad92624014e41da4cb1fa03f
Frankztr <frankzhangtr@gmail.com>
2021-09-07 10:05:40 +0800

    test Commit

--- src/Constants/Messages.java
+++ src/Constants/Messages.java
@@ -3,5 +3,6 @@ package Constants;
 public class Messages {
     public static final String WIN_GAME="Congratulations, you are the winner of this game";
     public static final String ERROR_OCCURRED="Sorry, an error has occurred";
-    
+    public static final String LOSS_GAME="You lost the game";
+
 }
