78609a84e949ce3b7d8336c7fea1394cbb7c8082
Frankztr <frankzhangtr@gmail.com>
2021-09-07 13:51:22 +0800

    Implement Comparable and set the Constructor of Card to Protected

--- src/Server/GameModule/Card.java
+++ src/Server/GameModule/Card.java
@@ -3,27 +3,31 @@ package Server.GameModule;
 import Constants.Messages;
 import Constants.Suits;
 
-public class Card {
+public class Card implements Comparable<Card>{
     private Suits suit;
     private int value;
-    public Card(Suits suit,int value){
-        if(value<1||value>13){
+
+    protected Card(Suits suit, int value) {
+        if (value < 1 || value > 13) {
             System.out.println(Messages.ERROR_OCCURRED);
-            System.out.println(Messages.ERROR_DECLARE+Messages.ERROR_TYPE_INVALID_CARD_NUMBER);
+            System.out.println(Messages.ERROR_DECLARE + Messages.ERROR_TYPE_INVALID_CARD_NUMBER);
             System.exit(0);
         }
-        this.suit=suit;
-        this.value=value;
+        this.suit = suit;
+        this.value = value;
     }
 
 
-    private Suits getSuit(){
+    private Suits getSuit() {
         return this.suit;
     }
-    private int getValue(){
+
+    private int getValue() {
         return this.value;
     }
 
-
-
+    @Override
+    public int compareTo(Card o) {
+        return Integer.compare(this.value, o.value);
+    }
 }
--- src/testMain.java
+++ src/testMain.java
@@ -1,9 +1,6 @@
-import Constants.Suits;
-import Server.GameModule.Card;
 
 public class testMain {
     public static void main(String[] args){
-        Card card=new Card(Suits.Clovers,15);
 
 
 
