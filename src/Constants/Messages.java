1b6279af6bbab2ca6497231219616849158d4f25
Frankztr <frankzhangtr@gmail.com>
2021-09-07 08:58:05 +0800

    Add Constants System

---
+++ src/Constants/Messages.java
@@ -0,0 +1,7 @@
+package Constants;
+
+public class Messages {
+    public static final String WIN_GAME="Congratulations, you are the winner of this game";
+    public static final String ERROR_OCCURRED="Sorry, an error has occurred";
+    
+}
